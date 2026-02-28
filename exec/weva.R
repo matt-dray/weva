308ecd6035ae0f65db5b68615fd7f411e8c587f1
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2026-02-28 23:35:48 +0000

    Introduce datetimes option, tweak docs

--- exec/weva.R
+++ exec/weva.R
@@ -5,14 +5,18 @@
 #| description: A UK postcode
 postcode <- NULL
 
-#| description: Hours hence for the future-look segment (up to three days)
+#| description: Hours from now for the 'later' segment (up to three days)
 #| short: h
 hours <- 1L
 
-#| description: Show segment for today's min- and max-temperature?
+#| description: Show API and calculated datetimes in 'now' and 'later' segments?
+#| short: d
+datetimes <- FALSE
+
+#| description: Show a segment for today's min and max temperatures?
 #| short: e
 extremes <- FALSE
 
 get_weather(postcode) |>
-  prepare_report(hours, extremes) |>
+  prepare_report(hours, datetimes, extremes) |>
   cat()
